51d559db44baef4c61209ab708c0e19ac754c784
Leonid Petrov <TaIbot1442@gmail.com>
2020-03-02 16:29:48 +0200

    add gulp plugin to minify CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+ 
+gulp.task('minify-css', () => {
+  return gulp.src('styles/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
+});
 
 function style() {
     return gulp.src('./scss/**/*.scss')
@@ -18,7 +25,6 @@ function watch() {
     gulp.watch('./scss/**/*.scss', style);
     gulp.watch('./*.html').on('change', browserSync.reload);
 }
-
 
 
 exports.style = style;
