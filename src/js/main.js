b6bd60b574aa1c6b58dcb40eb5e2643e59c42261
Leonid Petrov <TaIbot1442@gmail.com>
2020-03-12 22:30:32 +0200

    feat: fix bg-lines

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
     const modal = document.querySelector('.modal');
     const modalBtn = document.querySelectorAll('[data-toggle=modal]');
     const closeBtn = document.querySelector('.modal__close');
+    const modalContent = document.querySelector('.modal__dialog');
     const switchModal = () => {
         modal.classList.toggle('modal_visible');
     }
     modalBtn.forEach(element => {
-        element.addEventListener('click', switchModal);      
+        element.addEventListener('click', switchModal);    
     });
+
+    // modalContent.addEventListener('click', (e) =>{
+    //     if(e.target.className != '.modal__dialog'){
+    //         alert("target")
+    //     }
+    // });
     
     closeBtn.addEventListener('click', switchModal);
-
+    
+    document.addEventListener('keypress', (e) =>{
+        if(e.code === "KeyR"){
+            modal.classList.remove('modal_visible');
+        }
+    });
   });
