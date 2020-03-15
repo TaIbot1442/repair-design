3eb887436eba4b97496459c317ed938377e88884
Leonid Petrov <TaIbot1442@gmail.com>
2020-03-15 16:58:56 +0200

    feat:fix commit jquery code

--- src/js/main.js
+++ src/js/main.js
@@ -1,49 +1,49 @@
-// document.addEventListener("DOMContentLoaded", (event) =>{ 
-//   const modal = document.querySelector('.modal');
-//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-//   const closeBtn = document.querySelector('.modal__close');
+document.addEventListener("DOMContentLoaded", (event) =>{ 
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
  
-//   const switchModal = () => {
-//     modal.classList.toggle('modal_visible');
-//     if(modal.classList.contains('modal_visible')){
-//       document.addEventListener('keydown', pressESC);
-//     }
-//   };
+  const switchModal = () => {
+    modal.classList.toggle('modal_visible');
+    if(modal.classList.contains('modal_visible')){
+      document.addEventListener('keydown', pressESC);
+    }
+  };
 
-//   modalBtn.forEach(element => {
-//     element.addEventListener('click', switchModal);
-//   });
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal);
+  });
 
-//   closeBtn.addEventListener('click', switchModal);
+  closeBtn.addEventListener('click', switchModal);
 
-//   modal.addEventListener('click', (event)=>{
-//     if(event.target.classList.contains('modal')){
-//       modal.classList.remove('modal_visible');
-//     }
-//   });
+  modal.addEventListener('click', (event)=>{
+    if(event.target.classList.contains('modal')){
+      modal.classList.remove('modal_visible');
+    }
+  });
 
-//   const pressESC = (event) => {
-//     if(event.keyCode === 27){
-//       document.removeEventListener('keyup', pressESC);
-//       modal.classList.remove('modal_visible');
-//     }
-//   };
+  const pressESC = (event) => {
+    if(event.keyCode === 27){
+      document.removeEventListener('keyup', pressESC);
+      modal.classList.remove('modal_visible');
+    }
+  };
   
-// });
+});
 
 
 
   $(document).ready(function () {
-    var modal = $('.modal'),
-        modalBtn = $('[data-toggle=modal]'),
-        closeBtn = $('.modal__close');
+    // var modal = $('.modal'),
+    //     modalBtn = $('[data-toggle=modal]'),
+    //     closeBtn = $('.modal__close');
 
-    modalBtn.on('click', function () {
-        modal.toggleClass('modal_visible');
-    });
-    closeBtn.on('click',function () {
-        modal.toggleClass('modal_visible');
-    });  
+    // modalBtn.on('click', function () {
+    //     modal.toggleClass('modal_visible');
+    // });
+    // closeBtn.on('click',function () {
+    //     modal.toggleClass('modal_visible');
+    // });  
   
     var mySwiper = new Swiper ('.swiper-container', {      
       loop: true,
